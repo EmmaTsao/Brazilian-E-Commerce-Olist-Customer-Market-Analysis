e07f9507804ae1c3087bab9a1db10abec82b5a91
Emma Tsao <tttxyunn@gmail.com>
2025-09-07 18:50:19 +0800

    Update 03_verify_delivered_only_data_basis.sql

--- 03_verify_delivered_only_data_basis.sql
+++ 03_verify_delivered_only_data_basis.sql
@@ -3,20 +3,20 @@
 -- Purpose:
 -- 1. Summarize the number and proportion of orders by each status.
 -- 2. Compare the total payment value and total product price plus freight value across statuses.
--- 3. Identify which order statuses should be included in subsequent customer and sales analyses.
+-- 3. Identify which order statuses should be included in subsequent customer and revenue analyses.
 
 -- Background:
 -- 1. In this dataset, only 'delivered' orders represent fully completed transactions, with both shipment fulfillment and actual payment received.
 -- 2. Discrepancies between payment value and product value are expected in delivered orders, due to real-world business factors such as discounts, refunds, or installment payments.
 
 -- Analytical Implication:
--- 1. Only 'delivered' orders will be retained for subsequent customers and sales analysis, as they reflect genuine customer behavior and realized revenue.
+-- 1. Only 'delivered' orders will be retained for subsequent customers and revenue analysis, as they reflect genuine customer behavior and realized revenue.
 -- 2. Excluding other statuses improves data reliability and aligns results with actual commercial outcomes.
 
 -- Note:
 -- 1. Non-delivered orders may contain payment or item data, but usually do not reflect finalized transactions or actual cash flow.
 -- 2. Significant value differences (diffs) are only observed in 'delivered' orders, confirming their commercial completion.
--- 3. Although this verification includes freight value in the total order amount, subsequent customer and sales analyses will exclude freight to focus on true product sales revenue.
+-- 3. Although this verification includes freight value in the total order amount, subsequent customer and revenue analyses will exclude freight to focus on true product revenue.
 -- 4. Including freight here helps quantify its impact and better understand differences between payment value and product charges.
 
  WITH order_item_sum AS (
